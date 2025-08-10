0cb4e83e7411f4bfffb6f77fbbee3dbc79bdf1f9
kritika-neupane <kritikaneu32@gmail.com>
2025-08-10 12:51:20 +0545

    employee commit

--- employee/models.py
+++ employee/models.py
@@ -1,10 +1,8 @@
 from django.db import models
 
-# Create your models here.
-
-class Employee():
+class Employee(models.Model):
     name = models.CharField(max_length=100)
     position = models.CharField(max_length=100)
-    
+
     def __str__(self):
         return self.name
